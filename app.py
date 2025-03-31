d83bf3ff3080a7a2343b23ba1f546fe9e8e78631
davidcalleja-uofe <s2153222@ed.ac.uk>
2025-03-31 11:01:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import subprocess
+import os
 from xgboost import XGBClassifier  # Ensure xgboost is installed
 
 # ---------------------------
